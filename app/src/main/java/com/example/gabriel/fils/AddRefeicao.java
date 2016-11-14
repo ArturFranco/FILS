6728486ef38249e55e9cdfa835b6c8445d84e5b9
artuurfranco <anf@cin.ufpe.br>
2016-11-14 16:13:02 -0200

    Mensagem qualquer

--- app/src/main/java/com/example/gabriel/fils/AddRefeicao.java
+++ app/src/main/java/com/example/gabriel/fils/AddRefeicao.java
@@ -1,11 +1,15 @@
 package com.example.gabriel.fils;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -23,17 +27,38 @@ public class AddRefeicao extends AppCompatActivity {
         setContentView(R.layout.add_refeicao_main);
 
 
-        // Button upload = (Button) findViewById(R.id.uploadNovaRefeicao);
-        mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
+        Button upload = (Button) findViewById(R.id.uploadNovaRefeicao);
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
+                EditText texto = (EditText) findViewById(R.id.addRefeicaoText);
+                mFirebaseDatabaseReference.child("Refeições").push().setValue(texto.getText().toString());
 
-        EditText texto = (EditText) findViewById(R.id.addRefeicaoText);
+                Toast toast = Toast.makeText(AddRefeicao.this, "Refeição Salva",Toast.LENGTH_LONG);
+                toast.show();
+
+                finish();
+            }
+        });
 
     }
 
-    protected void uploadRefeicao (View view){
+   /* protected void uploadTreino (View view){
         mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
-        EditText texto = (EditText) findViewById(R.id.addRefeicaoText);
-        mFirebaseDatabaseReference.child("Refeições").push().setValue(texto.getText().toString());
+        EditText texto = (EditText) findViewById(R.id.addTreinoText);
+        mFirebaseDatabaseReference.child("Treinos").push().setValue(texto.getText().toString());
+
+        String toastText = "Treino Adicionado!";
+
+        Context context = getApplicationContext();
+
+        Toast toast = Toast.makeText(context, toastText,Toast.LENGTH_LONG);
+        toast.show();
+
+//        Intent intent = new Intent(AddTreino.this, NovoTreino.class);
+//        startActivity(intent);
     }
+*/
 
 }
--- app/src/main/java/com/example/gabriel/fils/NovaRefeicao.java
+++ app/src/main/java/com/example/gabriel/fils/NovaRefeicao.java
@@ -1,5 +1,9 @@
 package com.example.gabriel.fils;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
@@ -9,10 +13,16 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -24,10 +34,12 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
 /**
  * Created by Artur on 10/11/2016.
  *
- * Tela que lista as refeições cadastrados e permite casdastrar outras
+ * Tela que lista as refeições cadastrados e permite cadastrar novass
  */
 
 public class NovaRefeicao extends AppCompatActivity {
@@ -42,22 +54,58 @@ public class NovaRefeicao extends AppCompatActivity {
         //Pega referencia do banco de dados
         mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
 
-        //Pega a referencia da lista de refeicoes
-        mFirebaseDatabaseReference.child("Refeições").addListenerForSingleValueEvent(new ValueEventListener() {
+        //Pega a referencia da lista de treinos
+        mFirebaseDatabaseReference.child("Refeições").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(com.google.firebase.database.DataSnapshot dataSnapshot) {
                 //Referencia a listview do xml
                 ListView listaDeAtividades = (ListView) findViewById(R.id.listaAtividadesRef);
 
-                //Povoa uma lista com os nomes das refeições
+                //Povoa uma lista com os nomes dos treinos
                 Iterator<DataSnapshot> it = dataSnapshot.getChildren().iterator();
                 List<String> list = new ArrayList<String>();
                 while (it.hasNext())
                     list.add(it.next().getValue().toString());
 
                 //Atribui a lista de nomes a listview
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(NovaRefeicao.this,android.R.layout.simple_list_item_1, list);
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(NovaRefeicao.this, android.R.layout.simple_list_item_1, list);
                 listaDeAtividades.setAdapter(adapter);
+
+                listaDeAtividades.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        /*String entry= (String) parent.getAdapter().getItem(position);
+                        Toast toast = Toast.makeText(NovoTreino.this, entry.toString(),Toast.LENGTH_SHORT);
+                        //toast.setGravity();
+
+                        LayoutInflater inflater = getLayoutInflater();
+                        View layout = inflater.inflate(R.layout.popup_treino,
+                                (ViewGroup) findViewById(R.id.custom_popup_treino));
+                        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                        toast.setView(layout);
+                        toast.show();*/
+                        final Dialog dialog = new Dialog(NovaRefeicao.this);
+                        dialog.setTitle("Relatar Refeição");
+                        dialog.setContentView(R.layout.dialog_novarefeicao);
+
+                        String entry = (String) parent.getAdapter().getItem(position);
+
+                        TextView nome = (TextView) dialog.findViewById(R.id.refeicaoNome);
+
+
+                        Button botaoConfirma = (Button) dialog.findViewById(R.id.botaoConfirmaRef);
+                        botaoConfirma.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                dialog.dismiss();
+                            }
+                        });
+
+                        dialog.show();
+
+                        nome.setText(entry.toString());
+                    }
+                });
 
             }
 
@@ -68,7 +116,7 @@ public class NovaRefeicao extends AppCompatActivity {
         });
 
 
-        //TODO adicionar a funcao de adicionar novas refeições
+        //TODO adicionar a funcao de adicionar novos treinos
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.addRefeicaoButton);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,6 +126,13 @@ public class NovaRefeicao extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
 }
 
 
