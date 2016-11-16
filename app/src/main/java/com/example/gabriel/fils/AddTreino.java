908f445a1800e99162547e3bb32ecfb40cb3a1e4
DESKTOP-O8HDUT0\Gabriel <gfm@cin.ufpe.br>
2016-11-16 20:45:34 -0300

    - Troca de telas para tipos na adição de treinos

--- app/src/main/java/com/example/gabriel/fils/AddTreino.java
+++ app/src/main/java/com/example/gabriel/fils/AddTreino.java
@@ -8,7 +8,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
@@ -20,14 +23,17 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class AddTreino extends AppCompatActivity {
     private DatabaseReference mFirebaseDatabaseReference;
+    private int tipo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_treino_main);
+        setContentView(R.layout.add_treino_musculacao);
+        tipo = 1;
 
 
-        Button upload = (Button) findViewById(R.id.uploadNovoTreino);
+
+       /* Button upload = (Button) findViewById(R.id.uploadNovoTreino);
         upload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,25 +46,115 @@ public class AddTreino extends AppCompatActivity {
 
                 finish();
             }
-        });
+        });*/
+
+        /*ImageButton avancar = (ImageButton) findViewById(R.id.fowardAddTreino);
+        avancar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               setContentView(R.layout.add_treino_corrida);
+            }
+
+        });*/
 
     }
 
-   /* protected void uploadTreino (View view){
-        mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
-        EditText texto = (EditText) findViewById(R.id.addTreinoText);
-        mFirebaseDatabaseReference.child("Treinos").push().setValue(texto.getText().toString());
+   protected void uploadTreino (View view){
+       mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
+       EditText texto = (EditText) findViewById(R.id.addTreinoText);
+       mFirebaseDatabaseReference.child("Treinos").push().setValue(texto.getText().toString());
 
-        String toastText = "Treino Adicionado!";
+       Toast toast = Toast.makeText(AddTreino.this, "Treino Salvo",Toast.LENGTH_LONG);
+       toast.show();
 
-        Context context = getApplicationContext();
-
-        Toast toast = Toast.makeText(context, toastText,Toast.LENGTH_LONG);
-        toast.show();
+       finish();
 
 //        Intent intent = new Intent(AddTreino.this, NovoTreino.class);
 //        startActivity(intent);
     }
-*/
+
+    protected void passarTipoTreino (View view){
+        if(tipo == 1){
+            setContentView(R.layout.add_treino_corrida);
+            tipo = 2;
+        }else if(tipo == 2){
+            setContentView(R.layout.add_treino_marcial);
+            tipo = 3;
+        }else if(tipo == 3){
+            setContentView(R.layout.add_treino_outro);
+            tipo = 4;
+        }else{
+            setContentView(R.layout.add_treino_musculacao);
+            tipo = 1;
+        }
+    }
+
+    protected void voltarTipoTreino (View view){
+        if(tipo == 1){
+            setContentView(R.layout.add_treino_outro);
+            tipo = 4;
+        }else if(tipo == 2){
+            setContentView(R.layout.add_treino_musculacao);
+            tipo = 1;
+        }else if(tipo == 3){
+            setContentView(R.layout.add_treino_corrida);
+            tipo = 2;
+        }else{
+            setContentView(R.layout.add_treino_marcial);
+            tipo = 3;
+        }
+    }
+
+    protected void clickA(View view){
+        CheckBox checkB = (CheckBox) findViewById(R.id.checkBoxB);
+        CheckBox checkC = (CheckBox) findViewById(R.id.checkBoxC);
+        CheckBox checkD = (CheckBox) findViewById(R.id.checkBoxD);
+        CheckBox checkE = (CheckBox) findViewById(R.id.checkBoxE);
+        checkB.setChecked(false);
+        checkC.setChecked(false);
+        checkD.setChecked(false);
+        checkE.setChecked(false);
+    }
+    protected void clickB(View view){
+        CheckBox checkA = (CheckBox) findViewById(R.id.checkBoxA);
+        CheckBox checkC = (CheckBox) findViewById(R.id.checkBoxC);
+        CheckBox checkD = (CheckBox) findViewById(R.id.checkBoxD);
+        CheckBox checkE = (CheckBox) findViewById(R.id.checkBoxE);
+        checkA.setChecked(false);
+        checkC.setChecked(false);
+        checkD.setChecked(false);
+        checkE.setChecked(false);
+    }
+    protected void clickC(View view){
+        CheckBox checkB = (CheckBox) findViewById(R.id.checkBoxB);
+        CheckBox checkA = (CheckBox) findViewById(R.id.checkBoxA);
+        CheckBox checkD = (CheckBox) findViewById(R.id.checkBoxD);
+        CheckBox checkE = (CheckBox) findViewById(R.id.checkBoxE);
+        checkB.setChecked(false);
+        checkA.setChecked(false);
+        checkD.setChecked(false);
+        checkE.setChecked(false);
+    }
+    protected void clickD(View view){
+        CheckBox checkB = (CheckBox) findViewById(R.id.checkBoxB);
+        CheckBox checkC = (CheckBox) findViewById(R.id.checkBoxC);
+        CheckBox checkA = (CheckBox) findViewById(R.id.checkBoxA);
+        CheckBox checkE = (CheckBox) findViewById(R.id.checkBoxE);
+        checkB.setChecked(false);
+        checkC.setChecked(false);
+        checkA.setChecked(false);
+        checkE.setChecked(false);
+    }
+    protected void clickE(View view){
+        CheckBox checkB = (CheckBox) findViewById(R.id.checkBoxB);
+        CheckBox checkC = (CheckBox) findViewById(R.id.checkBoxC);
+        CheckBox checkD = (CheckBox) findViewById(R.id.checkBoxD);
+        CheckBox checkA = (CheckBox) findViewById(R.id.checkBoxA);
+        checkB.setChecked(false);
+        checkC.setChecked(false);
+        checkD.setChecked(false);
+        checkA.setChecked(false);
+    }
+
 
 }
