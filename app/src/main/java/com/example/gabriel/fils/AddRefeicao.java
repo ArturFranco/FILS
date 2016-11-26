7b120d5101d9db15ae3c37fff6944dbf7ec3e894
artuurfranco <anf@cin.ufpe.br>
2016-11-26 18:07:39 -0200

    Melhoras na tela de adicionar refeição

--- app/src/main/java/com/example/gabriel/fils/AddRefeicao.java
+++ app/src/main/java/com/example/gabriel/fils/AddRefeicao.java
@@ -1,114 +1,178 @@
 package com.example.gabriel.fils;
 
-import android.content.Context;
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
-import android.app.Activity;
-import android.widget.ExpandableListView;
-import android.widget.ExpandableListView.OnChildClickListener;
-import android.widget.ExpandableListView.OnGroupClickListener;
-import android.widget.ExpandableListView.OnGroupCollapseListener;
-import android.widget.ExpandableListView.OnGroupExpandListener;
 
 /**
- * Created by Artur on 14/11/2016.
+ * Created by Artur on 24/11/2016.
  */
 
 public class AddRefeicao extends AppCompatActivity {
     private DatabaseReference mFirebaseDatabaseReference;
-    ExpandableListAdapter listAdapter;
-    ExpandableListView expListView;
-    List<String> listDataHeader;
-    HashMap<String, List<String>> listDataChild;
+    private Spinner spinnerGruposAlimentares;
+    private Spinner spinnerAlimentos;
+    ArrayAdapter adapterAlimentos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_refeicao_main);
 
-        // get the listview
-        expListView = (ExpandableListView) findViewById(R.id.elvGruposAlimentares);
+        //Pega referencia do banco de dados
+        mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
 
-        // preparing list data
-        prepareListData();
+        spinnerGruposAlimentares = (Spinner) findViewById(R.id.spinnerGrupoAlimento);
 
-        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+        ArrayAdapter adapterGrupo = ArrayAdapter.createFromResource(this, R.array.grupos_alimentares , android.R.layout.simple_spinner_dropdown_item);
 
-        // setting list adapter
-        expListView.setAdapter(listAdapter);
+        spinnerGruposAlimentares.setAdapter(adapterGrupo);
 
-        /*Button upload = (Button) findViewById(R.id.uploadNovaRefeicao);
-        upload.setOnClickListener(new View.OnClickListener() {
+        spinnerAlimentos = (Spinner) findViewById(R.id.spinnerAlimento);
+
+        AdapterView.OnItemSelectedListener escolhaGrupo = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view , int position, long id) {
+
+                final String item = spinnerGruposAlimentares.getSelectedItem().toString();
+
+                System.out.println(item);
+
+                //Pega a referencia da lista de treinos
+                mFirebaseDatabaseReference.child("ALIMENTOS").addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(final com.google.firebase.database.DataSnapshot dataSnapshot) {
+
+                        //Povoa uma lista com os nomes dos treinos
+                        Iterator<DataSnapshot> it = dataSnapshot.getChildren().iterator();
+                        List<String> list = new ArrayList<String>();
+                        DataSnapshot aux;
+                        while (it.hasNext()) {
+                            aux = it.next();
+                            if(aux.child("Categoria").getValue().toString().equals(item))
+                                list.add(aux.child("Descrição dos alimentos").getValue().toString());
+                        }
+
+                        //Atribui a lista de nomes a listview
+                        ArrayAdapter<String> adapter = new ArrayAdapter<String>(AddRefeicao.this, android.R.layout.simple_spinner_dropdown_item, list);
+                        spinnerAlimentos.setAdapter(adapter);
+
+                        /*listaDeAtividades.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                                String entry = (String) parent.getAdapter().getItem(position);
+                                final Dialog dialog = new Dialog(NovoTreino.this);
+
+
+                                if(entry.startsWith("Corrida")){
+                                    dialog.setTitle("Relatar Treino");
+                                    dialog.setContentView(R.layout.dialog_novotreino_corrida);
+
+                                    TextView nome = (TextView) dialog.findViewById(R.id.treinoNome);
+                                    String s = entry.toString();
+                                    nome.setText(s.substring(s.lastIndexOf(":") + 1));
+
+
+                                    //DatabaseReference ref = mFirebaseDatabaseReference.child("Treino").child(s.substring(s.lastIndexOf(":") + 2));
+                                    TextView dur = (TextView) dialog.findViewById(R.id.duracaoText);
+                                    dur.setText(" " + dataSnapshot.child(s.substring(s.lastIndexOf(":") + 2)).child("Duracao").getValue().toString() + "min");
+
+                                    TextView dis = (TextView) dialog.findViewById(R.id.distText);
+                                    dis.setText(" " + dataSnapshot.child(s.substring(s.lastIndexOf(":") + 2)).child("Distancia").getValue().toString() + "km");
+
+                                }else if(entry.startsWith("Outro")){
+                                    dialog.setTitle("Relatar Treino");
+                                    dialog.setContentView(R.layout.dialog_novotreino_outro);
+
+                                    TextView nome = (TextView) dialog.findViewById(R.id.treinoNome);
+                                    String s = entry.toString();
+                                    nome.setText(s.substring(s.lastIndexOf(":") + 1));
+
+
+                                    //DatabaseReference ref = mFirebaseDatabaseReference.child("Treino").child(s.substring(s.lastIndexOf(":") + 2));
+                                    TextView dur = (TextView) dialog.findViewById(R.id.duracaoText);
+                                    dur.setText(" " + dataSnapshot.child(s.substring(s.lastIndexOf(":") + 2)).child("Duracao").getValue().toString() + "h");
+
+                                }
+
+                                //TODO relatar treino
+                                Button botaoConfirma = (Button) dialog.findViewById(R.id.botaoConfirma);
+                                botaoConfirma.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        dialog.dismiss();
+                                    }
+                                });
+
+                                //Cancela o relato de treino
+                                Button botaoCancela = (Button) dialog.findViewById(R.id.botaoCancela);
+                                botaoCancela.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        dialog.dismiss();
+                                    }
+                                });
+
+                                dialog.show();
+                            }
+                        });*/
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+
+                spinnerAlimentos.setAdapter(adapterAlimentos);
+                // Toast.makeText(getApplicationContext(), "Item escolhido: "+item,Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        };
+
+       spinnerGruposAlimentares.setOnItemSelectedListener(escolhaGrupo);
+
+
+        /*Button botao = (Button) findViewById(R.id.salvarGrupoButton);
+        botao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
-                EditText texto = (EditText) findViewById(R.id.addRefeicaoText);
-                mFirebaseDatabaseReference.child("Refeições").push().setValue(texto.getText().toString());
-
-                Toast toast = Toast.makeText(AddRefeicao.this, "Refeição Salva",Toast.LENGTH_LONG);
-                toast.show();
-
+                Intent returnIntent = getIntent();
+                EditText texto = (EditText) findViewById(R.id.descMusc);
+                returnIntent.putExtra("result", texto.getText().toString());
+                setResult(Activity.RESULT_OK, returnIntent);
                 finish();
             }
         });*/
-
     }
-
-    private void prepareListData() {
-        listDataHeader = new ArrayList<String>();
-        listDataChild = new HashMap<String, List<String>>();
-
-        // Adding child data
-        listDataHeader.add("Grupo A");
-        listDataHeader.add("Grupo B");
-        listDataHeader.add("Grupo C");
-
-        // Adding child data
-        List<String> grupoA = new ArrayList<String>();
-        grupoA.add("Arroz");
-        grupoA.add("Aveia");
-
-        List<String> grupoB = new ArrayList<String>();
-        grupoB.add("Alface");
-        grupoB.add("Tomate");
-
-        List<String> grupoC = new ArrayList<String>();
-        grupoC.add("Manteiga");
-
-        listDataChild.put(listDataHeader.get(0), grupoA); // Header, Child data
-        listDataChild.put(listDataHeader.get(1), grupoB);
-        listDataChild.put(listDataHeader.get(2), grupoC);
-    }
-
-   /* protected void uploadTreino (View view){
-        mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
-        EditText texto = (EditText) findViewById(R.id.addTreinoText);
-        mFirebaseDatabaseReference.child("Treinos").push().setValue(texto.getText().toString());
-
-        String toastText = "Treino Adicionado!";
-
-        Context context = getApplicationContext();
-
-        Toast toast = Toast.makeText(context, toastText,Toast.LENGTH_LONG);
-        toast.show();
-
-//        Intent intent = new Intent(AddTreino.this, NovoTreino.class);
-//        startActivity(intent);
-    }
-*/
-
 }
+
+
