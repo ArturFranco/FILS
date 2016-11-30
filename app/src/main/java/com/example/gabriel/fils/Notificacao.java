df3200c45b619ba8973fa23821bc007612f45e12
fsousad <fsousad@ncsu.edu>
2016-11-30 00:48:22 -0300

    - Salvando convite pendente do personal e do nutricionista - Salvando notificacao de convite

---
+++ app/src/main/java/com/example/gabriel/fils/Notificacao.java
@@ -0,0 +1,22 @@
+package com.example.gabriel.fils;
+
+/**
+ * Created by Felipe on 11/30/2016.
+ */
+
+public class Notificacao {
+    // pode ser:
+    // "Solicitacao Personal"
+    // "Solicitacao Nutricionista"
+    // "Solicitacao Aluno"
+    // "Meta"
+    // "Novo Treino"
+    // "Nova Refeicao"
+    public String descricao;
+    public String id; // pode identificar uma pessoal, um treino, uma dieta ou um atributo (para as metas)
+
+    public Notificacao(String descricao, String id) {
+        this.descricao = descricao;
+        this.id = id;
+    }
+}
