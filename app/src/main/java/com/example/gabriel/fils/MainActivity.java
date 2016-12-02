abc5e061907dee651fa6e11a2b9ba0cbcbbbadb3
DESKTOP-O8HDUT0\Gabriel <gfm@cin.ufpe.br>
2016-12-01 23:43:07 -0300

    - Retirada do botão settings - Retirada de botões desnecessários do slide menu - Mudança dos ícones do slide menu

--- app/src/main/java/com/example/gabriel/fils/MainActivity.java
+++ app/src/main/java/com/example/gabriel/fils/MainActivity.java
@@ -237,7 +237,7 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    @Override
+    /*@Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -250,7 +250,7 @@ public class MainActivity extends AppCompatActivity
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 
     //TODO
     @SuppressWarnings("StatementWithEmptyBody")
